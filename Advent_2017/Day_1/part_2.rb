cc2bd20a0c031ecbef95bb9f915ea03eabc1b62a
panagiotis <panagiotis.makropoulos@gmail.com>
2018-01-22 06:46:45 +0200

    Day_1 complete

---
+++ Advent_2017/Day_1/part_2.rb
@@ -0,0 +1,18 @@
+input = String.new
+File.open("day_1_input.txt", "r") do |f|
+  f.each_line do |line|
+    input += line
+  end
+end
+
+count = 0
+i = 0
+step = input.chars.length / 2
+next_el = 0
+while i != 2 * step do
+  i + step < 2 * step ? next_el = i + step : next_el = i - step
+  count += input.chars[i].to_i if input.chars[i] == input.chars[next_el]
+  i += 1 
+end
+
+puts count
