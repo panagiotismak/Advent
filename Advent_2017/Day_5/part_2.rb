503eb397cfd7e0346496afdf9393bbc5cbd8045f
panagiotis <panagiotis.makropoulos@gmail.com>
2018-01-23 06:53:30 +0200

    Day_5 completed

---
+++ Advent_2017/Day_5/part_2.rb
@@ -0,0 +1,19 @@
+input = Array.new
+
+File.open("input.txt", "r") do |f|
+  f.each_line do |line|
+    input << line.to_i
+  end
+end
+
+count = 0
+i = 0
+
+while i < input.length do
+  b = input[i]
+  b >= 3 ? input[i]-= 1 : input[i] += 1
+  i += b
+  count += 1
+end
+
+puts count
