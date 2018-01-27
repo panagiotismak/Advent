07df007f6ada3a3ddf378a80d04b2f923337a9a1
panagiotis <panagiotis.makropoulos@gmail.com>
2018-01-27 08:26:00 +0200

    Day_10 completed

---
+++ Advent_2017/Day_10/part_2.rb
@@ -0,0 +1,46 @@
+puzzle_input = Array.new
+
+File.open("input.txt", "r") do |f|
+  f.each_line do |line|
+    puzzle_input += line.split(//).map{ |x| x.codepoints }.flatten
+  end
+end
+
+my_list = (0..255).to_a
+skip_size = 0
+current_position = 0
+suffix = [17, 31, 73, 47, 23]
+puzzle_input += suffix
+
+64.times do
+  puzzle_input.each do |length|
+    if current_position + length < my_list.length
+      sublist = my_list[current_position, length]
+    else
+      sublist = my_list[current_position..-1] + my_list[0, length - my_list[current_position..-1].length]
+    end
+
+    sublist.reverse!
+
+    i = 0
+    sublist.each do |num|
+      if current_position + i < my_list.length
+        my_list[current_position + i] = num
+      else
+        my_list[current_position + i - my_list.length] = num
+      end
+      i += 1
+    end
+    
+    current_position += length + skip_size
+
+    if  current_position > my_list.length
+      until current_position < my_list.length do
+        current_position = current_position - my_list.length
+      end
+    end
+    skip_size += 1
+  end
+end
+
+puts my_list.each_slice(16).map{ |x| x.inject(:^).to_s(16).rjust(2, '0') }.inject(:+)
